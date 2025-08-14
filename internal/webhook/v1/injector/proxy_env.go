3a114d38cd8e5506f7810066c5a96ea6535974bf
suiyuan <thetallestlc@qq.com>
2025-08-14 09:08:04 +0800

    Add dfdaemon sock inject
    Signed-off-by: suiyuan <thetallestlc@qq.com>

--- internal/webhook/v1/injector/proxy_env.go
+++ internal/webhook/v1/injector/proxy_env.go
@@ -47,6 +47,7 @@ func (pei *ProxyEnvInjector) Inject(pod *corev1.Pod) {
 func (pei *ProxyEnvInjector) InjectContainer(c *corev1.Container) {
 	for _, e := range pei.Envs {
 		exsit := false
+		// if env exsit, skip
 		for _, ce := range c.Env {
 			if e.Name == ce.Name {
 				exsit = true
